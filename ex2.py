b711e43edd6baffaadad0d27030a9368aef20072
Brandon Price <brandon@brandon-mba.local>
2014-01-06 21:14:15 -0500

    adding initial 4 exercises

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#A comment
+print "I could have code like this" #and a comment afterwards
+
+#I could also use a comment to "disable" or comment out code
+#print "This won't run."
+
+print "This will run."
